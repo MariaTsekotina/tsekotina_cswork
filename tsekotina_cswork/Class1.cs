d999f9e4759d2a066205bad2f5f1ab3ec5b13601
opilane <opilane@HP-CZC6297B8F>
2022-02-09 13:13:36 +0200

    Add project files.

---
+++ tsekotina_cswork/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tsekotina_cswork
+{
+    class kordused_Massiivid
+    {
+
+        static void Main(string[] args)
+        {
+            char[] Tahed = new char[7] { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+            Console.WriteLine($"Esimene täht on {Tahed[0]}");
+
+            foreach (var taht in Tahed)
+            {
+                Console.Write($"{taht,}");
+            }
+
+            Random md = new Random();
+            int[] arvud = new int[10];
+            for (int i = 0; i < arvud.Length; i++)
+            {
+                arvud[i] = md.Next(5, 500);
+            }
+
+            foreach (int arv in arvud)
+            {
+                Console.Write($"{arv,4}");
+            }
+            int[,] tabel = new int[20, 10];
+            for (int i = 1; i < 21; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write($"rida {i} koht{j}");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
